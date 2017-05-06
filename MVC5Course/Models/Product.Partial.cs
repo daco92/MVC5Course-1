f7d5bc49a811fb91064d7ba3545af90e485f1b44
Will保哥 <doggy.huang@gmail.com>
2017-05-06 13:50:37 +0800

    10 用程式碼產生器範本來建立模型類別的 Buddy Class

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -1,29 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
-
 namespace MVC5Course.Models
 {
-    [MetadataType(typeof(ProductPartial))]
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    [MetadataType(typeof(ProductMetaData))]
     public partial class Product
     {
     }
-
-    public partial class ProductPartial
+    
+    public partial class ProductMetaData
     {
-        [Required(ErrorMessage = "請輸入商品名稱")]
-        [MinLength(3), MaxLength(30)]
-        [RegularExpression("(.+)-(.+)", ErrorMessage = "商品名稱格式錯誤")]
+        [Required]
+        public int ProductId { get; set; }
+        
+        [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
         public string ProductName { get; set; }
-        [Required]
-        [Range(0, 9999, ErrorMessage = "請設定正確的商品價格範圍")]
         public Nullable<decimal> Price { get; set; }
-        [Required]
         public Nullable<bool> Active { get; set; }
-        [Required]
-        [Range(0, 100, ErrorMessage = "請設定正確的商品庫存數量")]
         public Nullable<decimal> Stock { get; set; }
+    
+        public virtual ICollection<OrderLine> OrderLine { get; set; }
     }
 }
