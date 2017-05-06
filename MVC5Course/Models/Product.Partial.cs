38c0912039ce8933b302635037f7f111924187a6
Will保哥 <doggy.huang@gmail.com>
2017-05-06 14:12:16 +0800

    11 練習用 DisplayFormat 修改 Product 模型類別 Price 屬性的顯示格式

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -17,6 +17,7 @@ namespace MVC5Course.Models
         public string ProductName { get; set; }
         [Required]
         [Range(0, 9999, ErrorMessage = "請設定正確的商品價格範圍")]
+        [DisplayFormat(DataFormatString = "{0:0}")]
         public Nullable<decimal> Price { get; set; }
         [Required]
         public Nullable<bool> Active { get; set; }
