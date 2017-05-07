12f354853cc96f1eb5f0780ee2ca7bbbb33fa321
Will保哥 <doggy.huang@gmail.com>
2017-05-07 13:25:19 +0800

    17 練習透過導覽屬性刪除關聯資料表中的資料 ( Remove & RemoveRange )

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -71,6 +71,16 @@ namespace MVC5Course.Controllers
         public ActionResult Delete(int id)
         {
             var product = db.Product.Find(id);
+
+            /*
+            foreach (var item in product.OrderLine.ToList())
+            {
+                db.OrderLine.Remove(item);
+            }
+            */
+
+            db.OrderLine.RemoveRange(product.OrderLine);
+
             db.Product.Remove(product);
             db.SaveChanges();
 
