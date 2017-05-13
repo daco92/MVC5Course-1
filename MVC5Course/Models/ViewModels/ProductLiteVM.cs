212a88ea19d94be102af8ada1a15efd1f359bee6
Will保哥 <doggy.huang@gmail.com>
2017-05-13 18:13:10 +0800

    示範 /ClientsController/BatchUpdate 批次更新功能

--- MVC5Course/Models/ViewModels/ProductLiteVM.cs
+++ MVC5Course/Models/ViewModels/ProductLiteVM.cs
@@ -16,8 +16,10 @@ namespace MVC5Course.Models.ViewModels
         [MinLength(5)]
         public string ProductName { get; set; }
         [Required]
+        [Range(0, 9999)]
         public Nullable<decimal> Price { get; set; }
         [Required]
+        [Range(0, 9999)]
         public Nullable<decimal> Stock { get; set; }
     }
 }
